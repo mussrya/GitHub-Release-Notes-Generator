be05ea3b5f71bdab05056dfa91b75c02bae95ad0
mussrya <ryan.musselwhite@gmail.com>
2015-08-24 20:47:26 +0100

    building new login method

--- components/app.routes.js
+++ components/app.routes.js
@@ -3,6 +3,9 @@ app.config(function ($routeProvider) {
   when('/home', {
     templateUrl: 'components/home/home.html',
     controller: 'homeController'
+  }).when('/auth', {
+    templateUrl: 'components/auth/auth.html',
+    controller: 'authController'
   }).
   otherwise({
     redirectTo: '/home'
